7bf5af30db66efac4de58ed5299e148239cbbffa
Aaron Christianson <ninjaaron@gmail.com>
2017-02-24 12:06:06 +0100

    change description in setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,9 @@ setup(
     author='FID-Judaica, Goethe Universität',
     license='MLP 2.0/EUPL 1.1',
     author_email='a.christianson@ub.uni-frankfurt.de',
-    url='https://github.com/FID-Judaica/pica-parse.py',
-    description='rule-based algorithms for deromanizing Hebrew',
+    url='https://github.com/FID-Judaica/deromanize.py',
+    description='rule-based algorithms converting Romanized text to original '
+                'scripts',
     long_description=open('README.rst').read(),
     packages=['deromanize'],
 )
