10aa5f9e8aabc01ef38fc2e5f6a3fae3a2092a94
Aaron Christianson <ninjaaron@gmail.com>
2017-08-14 13:11:35 +0200

    things

--- deromanize/caching.py
+++ deromanize/caching.py
@@ -1,3 +1,21 @@
+# Copyright 2017, Goethe University
+#
+# This library is free software; you can redistribute it and/or
+# modify it either under the terms of:
+#
+#   the EUPL, Version 1.1 or – as soon they will be approved by the
+#   European Commission - subsequent versions of the EUPL (the
+#   "Licence"). You may obtain a copy of the Licence at:
+#   https://joinup.ec.europa.eu/software/page/eupl
+#
+# or
+#
+#   the terms of the Mozilla Public License, v. 2.0. If a copy of the
+#   MPL was not distributed with this file, You can obtain one at
+#   http://mozilla.org/MPL/2.0/.
+#
+# If you do not alter this notice, a recipient may use your version of
+# this file under either the MPL or the EUPL.
 import unicodedata
 
 
@@ -14,13 +32,13 @@ def strip_chars(rep_keyvalue, chars=set('ieaou')):
 
 
 def replacer_maker(simple_replacements, pair_replacements):
-    set_reps = {tuple(v): k for k, v in pair_replacements.items()}
+    pair_reps = {tuple(v): k for k, v in pair_replacements.items()}
 
     def replace(rep_keyvalue):
         for pair in rep_keyvalue:
             target = pair[1]
-            if pair in set_reps:
-                yield set_reps[pair], target
+            if pair in pair_reps:
+                yield pair_reps[pair], target
             else:
                 new = pair[0]
                 for k, v in simple_replacements.items():
@@ -52,8 +70,7 @@ class CacheObject:
         else:
             self.data = {}
             if seed:
-                for row in seed:
-                    self.add(*row)
+                self.update(seed)
 
     def __setitem__(self, source, target):
         self.add(source, target)
@@ -62,10 +79,20 @@ class CacheObject:
         current = self.data.setdefault(source, {}).setdefault(target, 0)
         self.data[source][target] = current + count
 
+    def update(self, matches):
+        for row in matches:
+            self.add(*row)
+
     def __getitem__(self, value):
         if isinstance(value, tuple):
             return self.data[value[0]][value[1]]
         return self.data[value]
+
+    def get(self, value, default=None):
+        try:
+            return self[value]
+        except KeyError:
+            return default
 
     def __iter__(self):
         for source, targets in self.data.items():
@@ -111,30 +138,35 @@ class CacheDB(CacheObject):
         return self.con.__exit__(type, value, traceback)
 
     def add(self, source, target, count=1):
-        with self.con:
-            self.cur.execute(
-                '''
-                INSERT OR REPLACE INTO deromcache VALUES (
-                ?,
-                ?,
-                COALESCE(
-                    (SELECT count FROM deromcache
-                        WHERE source = ? AND target = ?),
-                        0) + ?
-                )
-                ''', (source, target, source, target, count))
+        self.cur.execute(
+            '''
+            INSERT OR REPLACE INTO deromcache VALUES (
+            ?,
+            ?,
+            COALESCE(
+                (SELECT count FROM deromcache
+                    WHERE source = ? AND target = ?),
+                    0) + ?
+            )
+            ''', (source, target, source, target, count))
 
     def __getitem__(self, value):
         if isinstance(value, tuple):
             self.cur.execute(
                 'SELECT count FROM deromcache WHERE source = ? AND target = ?',
                 (value[0], value[1]))
-            return self.cur.fetchall()[0][0]
+            try:
+                return self.cur.fetchall()[0][0]
+            except IndexError:
+                raise KeyError('{!r} not found'.format(value))
 
         self.cur.execute(
             'SELECT target, count FROM deromcache WHERE source = ?',
             (value,))
-        return dict(self.cur.fetchall())
+        results = self.cur.fetchall()
+        if not results:
+                raise KeyError('{!r} not found'.format(value))
+        return dict(results)
 
     def __iter__(self):
         self.cur.execute('SELECT * FROM deromcache')
