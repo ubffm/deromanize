ab04fde9ead5413c280c783591f9ccdf03b44969
Aaron Christianson <ninjaaron@gmail.com>
2019-10-18 16:31:28 +0200

    testing something with manifest

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ setup(
     license="MLP 2.0/EUPL 1.1",
     author_email="a.christianson@ub.uni-frankfurt.de",
     url="https://github.com/FID-Judaica/deromanize.py",
+    include_package_data=True,
     description="rule-based algorithms converting Romanized text to original "
     "scripts",
     long_description=open("README.rst").read(),
     packages=["deromanize"],
     requires=["libaaron", "pyyaml", "sqlalchemy"],
-    include_package_data=True,
 )
